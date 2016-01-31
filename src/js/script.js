5d138be25706e05749b61b01922908cc128c9a4a
vUdav <v.udav@bk.ru>
2016-02-01 01:59:48 +0300

    add copied code on click and chenged modify class on element class

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,13 @@ $(document).ready(function() {
 		var propertyId = $(this).attr('href');
 		$(propertyId).toggleClass('code-container__element--pulse');
 	});
+
+	// copy to clipboard property and value then click on value
+	var clipboard = new Clipboard('.code-container__element--value');
+	clipboard.on('success', function(e) {
+		$('#copy-notify').html('<strong>'+e.text+'</strong><br>is copied!');
+		$('#copy-notify').addClass('copy-notify--success');
+		setTimeout(function(){$('#copy-notify').removeClass('copy-notify--success')}, 3000);
+		e.clearSelection();
+	});
 });
