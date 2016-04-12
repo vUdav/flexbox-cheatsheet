6acc2f57e83bdacbc132305ad344a9ced1f84ed9
Валера Любимов <v.liubimov@pugofka.com>
2016-04-12 16:55:25 +0300

    clipboard popup stack show

--- src/js/script.js
+++ src/js/script.js
@@ -20,9 +20,9 @@ function copy() {
 
 	var clipboard = new Clipboard('.code-container__element-value');
 	clipboard.on('success', function(e) {
-		$('#copy-notify').html('<strong>'+e.text+'</strong><br>is copied!');
-		$('#copy-notify').addClass('copy-notify--success');
-		setTimeout(function(){$('#copy-notify').removeClass('copy-notify--success')}, 3000);
+		var notifyBlock = '<div class="copy-notify__block copy-notify__block--success"><strong>'+e.text+'</strong><br>is copied!</div>';
+		$('#copy-notify').prepend(notifyBlock);
+		// setTimeout(function(){$('#copy-notify').removeClass('copy-notify--success')}, 3000);
 		e.clearSelection();
 	});
 }
